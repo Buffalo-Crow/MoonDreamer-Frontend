f8fd32eb8fa975e1699538bb875d16ec47b5a177
Buffalo-Crow <crowwalk@icloud.com>
2025-12-26 14:17:36 -0500

    update: moonSignDescriptions.js utils

--- src/utils/moonSignDescriptions.js
+++ src/utils/moonSignDescriptions.js
@@ -1,15 +1,15 @@
 export const moonSignDescriptions = {
   aries:
-    "Fiery and impulsive energy influences your dreams. The element of fire can illuminate or destroy depending on the perspective and proportions. This can be an incredible time to burn through what no longer serves you, illuminate what does, and SEE what you currently have. Aires can be stubborn but this stubborness when used in your dream work can be beneficial",
-  taurus: "Grounded, earthy vibes bring calm dreamscapes. Aesthetic brilliance mixed with a grounded undertone. All themes surrounding home, through all lenses may appear",
-  gemini: "Curiosity and duality shape your dream world. This airy sign can go either way, and your dreams can reflect that perfectly",
-  cancer: "Emotional and nurturing themes in your subconscious. What lies just below the surface of your consciousness and waking life?",
+    "Fiery and impulsive energy influences your dreams. The element of fire can illuminate or destroy depending on the perspective and proportions. This can be an incredible time to burn through what no longer serves you, illuminate what does, and SEE what you currently have. Aires can be stubborn but this stubborness when used in your dream work can be beneficial.",
+  taurus: "Grounded, earthy vibes bring calm dreamscapes. Aesthetic brilliance mixed with a grounded undertone. All themes surrounding home, through all lenses may appear. Recurring dreams may manifest during this moon sign.",
+  gemini: "Curiosity and duality shape your dream world. This airy sign can go either way, and your dreams can reflect that perfectly. Ground these dreams by communicating your highest truth with as little words as possible. ",
+  cancer: "Emotional and nurturing themes in your subconscious. What lies just below the surface of your consciousness and waking life? What boundaries keep you safe (in your shell) and what boundaries should you keep in your life- will become much clearer.",
   leo: "Bold and dramatic dream imagery surfaces in dreams. There may be aspects of yourself that you are longing to shine, let your dreams show you. Additionally, your heart of hearts will be shown to you. What you deeply care about and where your loyalties lie.",
   virgo: "Analytical and detail-focused dream messages. There may be more nuances that you are able to grasp more freely during this cycle of the moon. Clearer messages, clearer calculations, and clearer awareness of emotional themes in your life.",
-  libra: "Harmony and balance flow into your night visions. If you are imbalanced your dreams may illuminate where balance is needed most",
+  libra: "Harmony and balance flow into your night visions. If you are imbalanced your dreams may illuminate where balance is needed most. Make the most of this moon and use it to gain insight into how you can make important decisions in your life.",
   scorpio: "Deep, transformative emotional dream journeys. The depth of this water sign is a metaphor for the deep subconscious and spiritual insights you are able to gain at this time",
-  sagittarius: "Adventurous dream quests unfold. Travel, Truth, and gaining of sacred knowledge are available to you at this time.",
+  sagittarius: "Adventurous dream quests unfold. Travel, Truth, and gaining of sacred knowledge are available to you at this time. If you can dream better - you can do better.",
   capricorn: "Structured and disciplined dream insights. What is being communicated to you and how is it being communicated to you? This question reveals hidden talents through work.",
-  aquarius: "Innovative and future-oriented dreams. Where is your spirit rebelling against the worldly vision you have created for yourself. Trippy visuals and expansive dimensions.",
-  pisces: "Mystical and emotional dream landscapes. Spiritual messages come through, these messages might not be directly",
+  aquarius: "Innovative and future-oriented dreams. Here your spirit has free reign on rebelling against the worldly version you have created for yourself. Trippy visuals and expansive dimensions.",
+  pisces: "Mystical and emotional dream landscapes. Spiritual messages come through with extreme potency. Freedom of expression- creativity and depth filled dreams exist during this period of time. Analyzing with your mental mind what these dreams symbolize might not be your best move right now.",
 };
