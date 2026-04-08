fa532e14f0566ddcf8d61513bfdd336177277df9
Buffalo-Crow <crowwalk@icloud.com>
2026-04-08 13:13:57 -0400

    moonsignDescriptions upodated

--- src/utils/moonSignDescriptions.js
+++ src/utils/moonSignDescriptions.js
@@ -1,25 +1,63 @@
 export const moonSignDescriptions = {
-  aries:
-    "Fiery and impulsive energy influences your dreams. The element of fire can illuminate or destroy depending on the perspective and proportions. This can be an incredible time to burn through what no longer serves you, illuminate what does, and SEE what you currently have. Aires can be stubborn but this stubborness when used in your dream work can be beneficial",
-  taurus:
-    "Grounded, earthy vibes bring calm dreamscapes. Aesthetic brilliance mixed with a grounded undertone. All themes surrounding home, through all lenses may appear",
-  gemini:
-    "Curiosity and duality shape your dream world. This airy sign can go either way, and your dreams can reflect that perfectly",
-  cancer:
-    "Emotional and nurturing themes in your subconscious. What lies just below the surface of your consciousness and waking life?",
-  leo: "Bold and dramatic dream imagery surfaces in dreams. There may be aspects of yourself that you are longing to shine, let your dreams show you. Additionally, your heart of hearts will be shown to you. What you deeply care about and where your loyalties lie.",
-  virgo:
-    "Analytical and detail-focused dream messages. There may be more nuances that you are able to grasp more freely during this cycle of the moon. Clearer messages, clearer calculations, and clearer awareness of emotional themes in your life.",
-  libra:
-    "Harmony and balance flow into your night visions. If you are imbalanced your dreams may illuminate where balance is needed most",
-  scorpio: `Deep, transformative emotional dream journeys. The depth of this water sign is a metaphor for the deep subconscious 
-  and spiritual insights you are able to gain at this time. Treasures that lie in the depth of your spritual psyche & `,
+  aries: `Fiery and impulsive energy influences your dreams. 
+    The element of fire can illuminate or destroy depending on the perspective and proportions.
+    This can be an incredible time to burn through what no longer serves you, illuminate what does,
+    and SEE what you currently have. 
+    Aires can be stubborn but this stubborness when used in your dream work can be beneficial. 
+    Stubbornly cut through the BS straight to the heart of the matter.`,
+  
+  taurus: `Grounded, earthy vibes bring calm dreamscapes. 
+    Aesthetic brilliance mixed with a grounded undertone. 
+    All themes surrounding home, through all lenses may appear. 
+    Additionally this moon can show you what is truly close to
+    home and close to the heart without the ego getting in the way.`,
+  
+  gemini: `Curiosity and duality shape your dream world. 
+    This airy sign can go either way, and your dreams can reflect that perfectly.
+    Gemini is the great communicator. These dreams can reveal ways in which you are ignoring
+    how you truly communicate with yourself and the world around you.`,
+  
+  cancer: `Emotional and nurturing themes in your subconscious. 
+    What lies just below the surface of your consciousness and waking life?
+    What makes you hide in your shell? Is it time to reemerge or reevaluate your boundaries.`,
+
+  leo: `Bold and dramatic dream imagery surfaces in dreams. 
+  There may be aspects of yourself that you are longing to shine, 
+  let your dreams show you. Additionally, your heart of hearts will be shown to you.
+  What you deeply care about and where your loyalties lie.`,
+
+  virgo: `Analytical and detail-focused dream messages. 
+    There may be more nuances that you are able to grasp more freely during this cycle of the moon. 
+    Clearer messages, clearer calculations, and clearer awareness of emotional themes in your life.`,
+  
+  libra:`Harmony and balance flow into your night visions. 
+    If you are imbalanced your dreams may illuminate where balance is needed most.
+    Where you are indescisive will be revealed with dramatic affect. 
+    Not choosing a side is also choosing a side. Create pathways of peace.`,
+  
+  scorpio: `Deep, transformative emotional dream journeys. 
+  The depth of this water sign serve as a metaphor for the deep subconscious 
+  and spiritual intuitions you are able to feel and see. 
+  Treasures that lie in the deep reveal insights from your spritual psyche  `,
+  
   sagittarius:
-    "Adventurous dream quests unfold. Travel, Truth, and gaining of sacred knowledge are available to you at this time.",
+    `Adventurous dream quests unfold, alternate universes and timelines.
+    Travel, truth and gaining of sacred knowledge are available to you with this moon sign. 
+    Hidden sources of joy may be revealed to you through spiritual metaphors and blunt symbolism. `,
+  
   capricorn:
-    "Structured and disciplined dream insights. What is being communicated to you and how is it being communicated to you? This question reveals hidden talents through work.",
+    `Structured and disciplined dream insights. 
+    What is being communicated to you and how is it being communicated to you? 
+    The answers to this question reveals hidden ways to translate your dream space into actionable waking life steps.`,
+  
   aquarius:
-    "Innovative and future-oriented dreams. Where is your spirit rebelling against the worldly vision you have created for yourself. Trippy visuals and expansive dimensions.",
+    `Innovative and future-oriented dreams.
+     Where is your spirit rebelling against the worldly vision you have created for yourself? 
+    Trippy visuals and expansive dimensions, and futuristic color schemes are used to translate
+    spiritual concepts that can be not only beneficial for you but the collective you belong to.`,
+  
   pisces:
-    "Mystical and emotional dream landscapes. Spiritual messages come through, these messages might not be directly",
+    `Mystical and emotional dream landscapes. 
+    Spiritual messages come through, dont take these signs likel, Pisces is the most direct route to spirit.
+    Witness yourself and the deepest of your emotional world. Surprise, shock, desire and wisdom.`,
 };
